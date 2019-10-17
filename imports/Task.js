f1b6202d20c50da154676cd982643db221fa77bb
Felipe Cruz <felipegomesdcruz@gmail.com>
2019-10-17 15:05:51 -0400

    Projeto microblog

---
+++ imports/Task.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Tasks } from '../tasks.js';
+
+// Task component - represents a single todo item
+export default class Task extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+  }
+
+  deleteThisTask() {
+    Meteor.call('tasks.remove', this.props.task._id);
+  }
+  render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
+      </li>
+    );
+  }
+}
---
+++ imports/tasks.js
@@ -0,0 +1,7 @@
+
+
+import { Mongo } from 'meteor/mongo';
+
+
+
+export const Tasks = new Mongo.Collection('tasks');
